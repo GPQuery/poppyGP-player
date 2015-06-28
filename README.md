a70cc2d01798b4551ad46b7cb3301aa675956004
loljoho <jho1290@smail.raritanval.edu>
2015-06-27 22:10:56 -0400

    Fixing branches...

---
+++ README.md
@@ -0,0 +1,9 @@
+# poppyGP-Player
+
+> A motorsports community-centered video channel and IRC webchat web application.
+
+
+## OVERVIEW
+
+
+
---
+++ src/app/core/view.js
@@ -0,0 +1,17 @@
+(function() {
+'use strict';
+
+angular.module('poppyGP.core')
+  .controller('View', View);
+
+function View($mdSidenav) {
+  var vm = this;
+
+  vm.openMenu = openMenu;
+
+  function openMenu() {
+    
+  }
+}
+
+})();
