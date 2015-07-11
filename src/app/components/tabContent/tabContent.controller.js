9f5d0ac6caa94423ed42897ec3f3cda7683e457f
loljoho <jho1290@smail.raritanval.edu>
2015-07-11 15:29:55 -0400

    Add gp-tab-content directive

---
+++ src/app/components/tabContent/tabContent.controller.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('poppyGP')
+    .controller('TabContentController', TabContentController);
+
+  /** @ngInject */
+  function TabContentController($timeout, $log) {
+    var vm = this;
+
+    vm.tabsContent = [];
+
+    activate();
+
+    function activate() {
+      getAllTabs();
+      $timeout(function() {
+        $log.info('Activated TabContentController');
+      }, 4000);
+    }
+
+    function getAllTabs()   {
+      vm.tabsContent = [{
+        'label'   : '/r/poppyGP',
+        'icon'    : 'fa-reddit',
+        'template': '<iframe class="reddit-frame" layout-fill src="https://reddit.com/r/poppygp"></iframe>'
+      }, {
+        'label'   : '#f1 on Snoonet',
+        'icon'    : 'fa-comments',
+        'template': '<iframe class="webchat-frame" layout-fill src="https://kiwiirc.com/client/irc.snoonet.org/?nick=poppygp-|?&theme=mini#f1"></iframe>'
+      }, {
+        'label'   : '@poppyGrandPrix',
+        'icon'    : 'fa-twitter',
+        'template': '<iframe class="twitter-frame" layout-fill src="https://twitter.com"></iframe>'
+      }];
+    }
+
+  }
+
+
+})();
